f560b8087633d544bdfb9d9fd9f6f48888497746
David Lara <eldark17@gmail.com>
2024-06-17 04:14:05 -0600

    Clean code

--- app/src/main/java/com/funapps/themovie/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/funapps/themovie/ui/maps/MapsFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.funapps.themovie.R
@@ -22,7 +21,6 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import java.util.Date
 
 
